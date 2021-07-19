4cd2cd7279a8bd98090ff9c73e50a9a99aca5eb8
yangmc <os.yangmc@ffcs.cn>
2021-07-19 22:32:10 +0800

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,6 +13,7 @@ public class HelloWorld implements IHelloWorld {
 
     public static void main(String[] args) {
         ArrayList<Object> objects = new ArrayList<>();
+        System.out.println("second commit");
     }
 
 
