7dfd815927cce4445dd03cf628a80fdd0b4c7bc7
yangmc <os.yangmc@ffcs.cn>
2021-07-19 22:26:54 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.OptionalInt;
+
+public class HelloWorld implements IHelloWorld {
+
+    //不含税承担金额=协议总价不含税价*（退款金额/支付金额）
+    private Long getAmountExcludingTax(Long protocolPrice, Long refundAmount, Long paymentAmount, Long rate) {
+        System.out.println(protocolPrice.doubleValue() * refundAmount.doubleValue() * 100.0);
+        System.out.println(protocolPrice.doubleValue() * refundAmount.doubleValue() * 100.0 / paymentAmount.doubleValue());
+        System.out.println(protocolPrice.doubleValue() * refundAmount.doubleValue() * 100.00 / paymentAmount.doubleValue() / (100.00 + rate.doubleValue()));
+        return Math.round(protocolPrice.doubleValue() * refundAmount.doubleValue() * 100.00 / paymentAmount.doubleValue() / (100.00 + rate.doubleValue()));
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+    }
+
+
+    @Override
+
+    public void test() {
+        boolean isNull = true;
+        if (!isNull) {
+
+        }
+        int i = 0;
+        OptionalInt.of(i);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/HelloWorldTest.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class HelloWorldTest {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+        int a;
+        System.out.println("a = ");
+        System.out.println("HelloWorldTest.main");
+    }
+    public void method(){
+        System.out.println("HelloWorldTest.method");
+        ArrayList<String> strList = new ArrayList<>();
+        if (strList != null) {
+
+        }
+
+    }
+}
